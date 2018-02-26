abd4a14c3266f5c289dbd42736d469c36ed9821b
cancel <sl888amg@gmail.com>
2018-02-26 22:47:07 +0300

    Task2

--- src/main/scala/crypto/Curve25519SignatureForger.scala
+++ src/main/scala/crypto/Curve25519SignatureForger.scala
@@ -4,6 +4,9 @@ import scorex.crypto.signatures.Signature
 
 object Curve25519SignatureForger {
 
-  def forgeSignature(signature: Signature): Signature = ???
+    def forgeSignature(signature: Signature): Signature = {
+        val modif: BigInt = BigInt("7237005577332262213973186563042994240857116359379907606001950938285454250989")
+        Signature @@ (signature.take(32) ++ (BigInt(signature.takeRight(32).reverse) + modif).toByteArray.reverse)
+    }
 
 }
